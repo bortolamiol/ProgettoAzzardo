46346c6394ea45ba91c59049210e82dbcedf5498
Alessandro <Alessandro@Bartolomeo>
2016-03-03 08:55:33 +0100

    08:55  --- 03/03/2016

--- ProgettoAzzardo/src/ProgettoAzzardo/Azzardo.java
+++ ProgettoAzzardo/src/ProgettoAzzardo/Azzardo.java
@@ -6,10 +6,5 @@ import org.eclipse.swt.graphics.Image;
 
 public class Azzardo {
 	
-	
-	
-	public int NumeroRotazione(){
-		int n = (int)Math.random()*11;
-		return n;
-	}
+		
 }
--- ProgettoAzzardo/src/ProgettoAzzardo/ProgettoAzzardoGrafica.java
+++ ProgettoAzzardo/src/ProgettoAzzardo/ProgettoAzzardoGrafica.java
@@ -19,6 +19,8 @@ import org.eclipse.swt.events.MouseAdapter;
 
 public class ProgettoAzzardoGrafica {
 	Image[] immagini = new Image[7];
+	Label lblSimbolo1, lblSimbolo2, lblSimbolo3;
+	
 	int n = 0;
 
 	protected Shell shell;
@@ -59,7 +61,7 @@ public class ProgettoAzzardoGrafica {
 	protected void createContents() {
 		shell = new Shell();
 
-		shell.setSize(450, 300);
+		shell.setSize(650, 500);
 		shell.setText("SWT Application");
 
 		// diamo ad ogni spazio del vettore un'immagine
@@ -79,38 +81,38 @@ public class ProgettoAzzardoGrafica {
 			public void widgetSelected(SelectionEvent e) {
 			}
 		});
-		btnReset.setBounds(10, 205, 75, 25);
+		btnReset.setBounds(30, 351, 75, 25);
 		btnReset.setText("Reset");
 
 		Button btnBetMax = new Button(shell, SWT.NONE);
-		btnBetMax.setBounds(107, 205, 75, 25);
+		btnBetMax.setBounds(141, 351, 75, 25);
 		btnBetMax.setText("Bet max");
 
 		Button btnBetOne = new Button(shell, SWT.NONE);
-		btnBetOne.setBounds(201, 205, 75, 25);
+		btnBetOne.setBounds(285, 351, 75, 25);
 		btnBetOne.setText("Bet One");
 
 		Label lblSimbolo2 = new Label(shell, SWT.BORDER);
 		lblSimbolo2.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblSimbolo2.setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
 		lblSimbolo2.setFont(SWTResourceManager.getFont("Segoe UI", 13, SWT.NORMAL));
-		lblSimbolo2.setBounds(165, 90, 88, 62);
+		lblSimbolo2.setBounds(257, 90, 160, 153);
 
 		Label lblSimbolo1 = new Label(shell, SWT.BORDER);
 		lblSimbolo1.setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
 		lblSimbolo1.setFont(SWTResourceManager.getFont("Segoe UI", 13, SWT.NORMAL));
 		lblSimbolo1.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		lblSimbolo1.setBounds(58, 90, 88, 62);
+		lblSimbolo1.setBounds(58, 90, 160, 153);
 
 		Label lblSimbolo3 = new Label(shell, SWT.BORDER);
 		lblSimbolo3.setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
 		lblSimbolo3.setFont(SWTResourceManager.getFont("Segoe UI", 13, SWT.NORMAL));
 		lblSimbolo3.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		lblSimbolo3.setBounds(272, 90, 88, 62);
+		lblSimbolo3.setBounds(451, 90, 160, 153);
 
 		Label lblNewLabel = new Label(shell, SWT.NONE);
 		lblNewLabel.setFont(SWTResourceManager.getFont("Segoe UI", 24, SWT.NORMAL));
-		lblNewLabel.setBounds(30, 10, 363, 41);
+		lblNewLabel.setBounds(30, 10, 497, 54);
 		lblNewLabel.setText("NARDI'S SLOT MACHINE");
 
 		Label lblSpin = new Label(shell, SWT.BORDER);
@@ -118,6 +120,7 @@ public class ProgettoAzzardoGrafica {
 		lblSpin.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
+				lblSpin.setEnabled(false);
 				Thread thread = new Thread() {
 					@Override
 					public void run() {
@@ -130,12 +133,11 @@ public class ProgettoAzzardoGrafica {
 								public void run() {
 										//ora le immagini cambiano
 									lblSimbolo1.setImage(immagini[n]);
-									lblSimbolo2.setImage(immagini[n]);
-									lblSimbolo3.setImage(immagini[n]);
+									
 								}
 							});
 							try {
-								Thread.sleep(250);
+								Thread.sleep(100);
 							} catch (InterruptedException e1) {
 								// TODO Auto-generated catch block
 								e1.printStackTrace();
@@ -145,7 +147,71 @@ public class ProgettoAzzardoGrafica {
 					}
 
 				};
+				
 				thread.start();
+				
+				
+				Thread thread2 = new Thread() {
+					@Override
+					public void run() {
+						// lblSpin.setBackground(SWTResourceManager.getColor(SWT.COLOR_YELLOW));
+
+						for (int i = 0; i < 7; i++) {
+							n = i;
+							System.out.println(i);
+							Display.getDefault().asyncExec(new Runnable() {
+								public void run() {
+										//ora le immagini cambiano
+									
+									lblSimbolo2.setImage(immagini[n]);
+									
+								}
+							});
+							try {
+								Thread.sleep(50);
+							} catch (InterruptedException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+
+					}
+
+				};
+				thread2.start();
+				//p.CambioImmagine(100);
+				
+				
+				Thread thread3 = new Thread() {
+					@Override
+					public void run() {
+						// lblSpin.setBackground(SWTResourceManager.getColor(SWT.COLOR_YELLOW));
+
+						for (int i = 0; i < 7; i++) {
+							n = i;
+							System.out.println(i);
+							Display.getDefault().asyncExec(new Runnable() {
+								public void run() {
+										//ora le immagini cambiano
+									
+									lblSimbolo3.setImage(immagini[n]);
+									
+								}
+							});
+							try {
+								Thread.sleep(60);
+							} catch (InterruptedException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+
+					}
+
+				};
+				thread3.start();
+				
+				//lblSpin.set(true);
 			}
 
 			public void mouseUp(MouseEvent e) {
@@ -162,13 +228,23 @@ public class ProgettoAzzardoGrafica {
 			public void mouseExit(MouseEvent e) {
 				lblSpin.setBackground(SWTResourceManager.getColor(SWT.COLOR_GREEN));
 			}
-		});// quanto forte è nardi????
+		});
 		lblSpin.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.BOLD));
 		lblSpin.setBackground(SWTResourceManager.getColor(SWT.COLOR_GREEN));
 		lblSpin.setImage(SWTResourceManager.getImage(ProgettoAzzardoGrafica.class, "/ProgettoAzzardo/PlayNow.png"));
-		lblSpin.setBounds(319, 202, 88, 32);
+		lblSpin.setBounds(464, 343, 88, 32);
 		lblSpin.setText("SPIN");
+		
+		
 
+	}
+	
+	public Image[] getImmagini() {
+		return immagini;
+	}
+
+	public void setImmagini(Image[] immagini) {
+		this.immagini = immagini;
 	}
 
 }
